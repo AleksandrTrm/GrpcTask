be716999aa80237da8d897e62d843d9a5d4a9499
AleksandrTrm <145889940+AleksandrTrm@users.noreply.github.com>
2024-10-10 21:53:24 +0500

    Migrations added.

---
+++ src/ConsoleClient/Program.cs
@@ -0,0 +1,23 @@
+﻿using ConsoleClient.Models;
+
+public class Program
+{
+    public static async Task Main(string[] args)
+    {
+        var messageSender = new MessageSender();
+        
+        var replies = await messageSender.SendMessage();
+
+        for (int i = 1; i < replies.Count + 1; i++)
+        {
+            Console.WriteLine($"Reply - {i}");
+            
+            Console.WriteLine(replies[i - 1].Error);
+            Console.WriteLine(replies[i - 1].IsSuccess);
+            
+            Console.WriteLine();
+        }
+
+        Console.ReadKey();
+    }
+}
---
+++ src/Server/Migrations/20241010150145_Initial.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "grpc_data",
+                columns: table => new
+                {
+                    packet_seq_num = table.Column<int>(type: "integer", nullable: false),
+                    record_seq_num = table.Column<int>(type: "integer", nullable: false),
+                    packet_timestamp = table.Column<long>(type: "bigint", nullable: false),
+                    decimal1 = table.Column<string>(type: "text", nullable: false),
+                    decimal2 = table.Column<string>(type: "text", nullable: false),
+                    decimal3 = table.Column<string>(type: "text", nullable: false),
+                    decimal4 = table.Column<string>(type: "text", nullable: false),
+                    record_timestamp = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_grpc_data", x => new { x.packet_seq_num, x.record_seq_num });
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "grpc_data");
+        }
+    }
+}
